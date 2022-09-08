0818b23edc6260e111034aac8ac17e9573a2dcaa
Steven Chiou <schiou@utdallas.edu>
2022-09-08 18:47:56 -0500

    pgee

--- PGEE.R
+++ PGEE.R
@@ -1,45 +1,158 @@
 library(Rcpp)
 library(microbenchmark)
 
+## Assume family = gaussian for our cure models
+## Assume fi = 1 because of the gaussian assumption
 sourceCpp(file = "PGEE.cpp")
 
 ## ##################################################
-## No cluster
-## ##################################################
-load("test.RData")
-y <- test$y
-mu <- test$mu
-X <- test$X
-Rhat <- test$Rhat
-N <- test$N
-nx <- test$nx
-nt <- test$nt
-index <- test$index
-muEta <- test$muEta
-
-out <- SHM(y, mu, X, Rhat, N, nx, nt, index, muEta)
-
-## ##################################################
-## With cluster
+## Generate example data
 ## ##################################################
 
-load("test2.RData")
-y <- test2$y
-mu <- test2$mu
-X <- test2$X
-Rhat <- test2$Rhat
-N <- test2$N
-nx <- test2$nx
-nt <- test2$nt
-index <- test2$index
-muEta <- test2$muEta
+library(mvtnorm)
+n <- 2000; pn <- 10; m <- 4
+id.vect <- rep(1:n, each = m) 
+X.sigma <- matrix(0,(pn-1),(pn-1))
+for (i in 1:(pn-1))
+  X.sigma[i,] <- 0.5^(abs((1:(pn-1))-i))  
+x.mat <- as.matrix(rmvnorm(n*m, mean = rep(0,(pn-1)), X.sigma))
+x.mat <- cbind(rbinom(n*m,1, 0.5), x.mat)
+beta.true <- c(2,3,1.5,2,rep(0,6))
+sigma2 <- 1
+rho <- 0.5
+R <- matrix(rho,m,m)+diag(rep(1-rho,m))
+SIGMA <- sigma2*R
+error <- rmvnorm(n, mean = rep(0,m),SIGMA)
+y.temp <- x.mat%*%beta.true
+y.vect <- y.temp+as.vector(t(error))    
+mydata <- data.frame(id.vect,y.vect,x.mat) 
+colnames(mydata) <- c("id","y",paste("x",1:length(beta.true),sep = ""))
 
-str(test2)
+formula <- "y ~.-id-1"
+data <- mydata
+family <- gaussian(link = "identity")
+lambda.vec <- seq(0.1,1,0.1)
+lambda <- 0.1    
+
+myfit1 <- PGEE(formula = formula, id = id, data = data, na.action = NULL, 
+               family = family, corstr = "exchangeable", Mv = NULL, 
+               beta_int = c(rep(0,length(beta.true))), R = NULL, scale.fix = TRUE, 
+               scale.value = 1, lambda = lambda, pindex = NULL, eps = 10^-6, maxiter = 30, 
+               tol = 10^-3, silent = TRUE)
+
+## ##################################################
+## Test
+## ##################################################
+
+load("testSHEM.RData")
+for (i in 1:length(test))
+  eval(parse(text = paste(names(test)[i], " <- test$", names(test)[i], sep = "")))
+
+library(modifiedPGEE)
+debug(S_H_E_M)
+out <- S_H_E_M(N, nt, y, X, nx, family, beta_new, Rhat, fihat, lambda, pindex, eps = 10^-6) 
+str(out)
+
+pindex <- rep(0, ncol(X))
+out2 <- SHEM(y, X, beta_new, Rhat, nt, pindex, lambda, eps)
+str(out2)
+
+sourceCpp(file = "PGEE.cpp")
+out <- gee(y, X, beta_new, Rhat, nt, pindex, lambda, eps, 1e-7, 100)
+str(out)
+
+e
+
+
+
+
+
 
 out1 <- SHM1(y, mu, X, Rhat, N, nx, nt, index, muEta)
-out2 <- SHM2(y, mu, X, Rhat, N, nx, nt, index, muEta)
+out2 <- SHM(y, mu, X, Rhat, N, nx, nt, index, muEta)
 str(out1)
 str(out2)
 
 all.equal(out1, out2)
-microbenchmark(out1, out2)
+microbenchmark(SHM1(y, mu, X, Rhat, N, nx, nt, index, muEta),
+               SHM(y, mu, X, Rhat, N, nx, nt, index, muEta))
+
+
+sourceCpp(code = '
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+using namespace arma;
+// [[Rcpp::export]]
+arma::mat r3(arma::mat A, arma::mat B, arma::mat C) {
+  return(A.t() * B * C * C.t() * B * A);
+}
+// [[Rcpp::export]]
+arma::mat r4(arma::mat A, arma::mat B, arma::mat C) {
+  arma::mat tmp = A.t() * B * C;
+  return(tmp * tmp.t());
+}')
+
+n <- 100
+A1 <- matrix(runif(n * n), n, n)
+A2 <- matrix(runif(n * n), n, n)
+A3 <- matrix(runif(n * n), n, n)
+
+all.equal(r3(A1, A2, A3), r4(A1, A2, A3))
+microbenchmark(r3(A1, A2, A3), r4(A1, A2, A3))
+
+
+sourceCpp(code = '
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+using namespace arma;
+// [[Rcpp::export]]
+arma::vec qscad(arma::vec b, double lambda) {
+  double a = 3.7;
+  arma::vec dif = a * lambda - b;
+  dif.elem(find(dif < 0)).zeros();
+  arma::vec out(b.n_elem);
+  out.fill(lambda);
+  arma::uvec ind = find(b > lambda);
+  out(ind) = dif(ind) / (a - 1);
+  return(out);
+}')
+
+r3(1:4)
+cumsum(1:3)
+
+qscad(abs(beta_new), lambda)
+q_scad(abs(c(beta_new)), lambda)
+
+microbenchmark(qscad(abs(beta_new), lambda),
+               q_scad(abs(c(beta_new)), lambda))
+
+sourceCpp(code = '
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
+using namespace arma;
+Rcpp::List r2(double a) {
+  Rcpp::List out(4);
+  arma::mat M1(1, 3, arma::fill::zeros);
+  arma::mat M2(2, 3, arma::fill::zeros);
+  arma::mat M3(3, 3, arma::fill::zeros);
+  arma::mat M4(4, 3, arma::fill::zeros);
+  out(0) = M1;
+  out(1) = M2;
+  out(2) = M3;
+  out(3) = M4;
+  return(out);
+}
+// [[Rcpp::export]]
+arma::vec r3(double a) {
+  arma::vec out(10);
+  out.fill(a);
+  Rcpp::List tmp = r2(a);
+  arma::mat m = tmp(1);
+  std::cout << m;
+  return(out);
+}')
+
+r3(3)
+
+
+q_scad(abs(c(beta_new)), lambda) / (abs(as.vector(beta_new)) + eps)
--- PGEE.cpp
+++ PGEE.cpp
@@ -18,63 +18,138 @@ arma::mat matvec(arma::mat x, arma::vec y) {
 
 //' @noRd
 // [[Rcpp::export(rng = false)]]
-Rcpp::List SHM1(arma::vec y, arma::vec mu, arma::mat X,
-								arma::cube Rhat, int N, int nx, arma::vec nt,
-								arma::vec index, arma::vec muEta){
-	Rcpp::List out(3);
-	arma::mat sumS = zeros<mat>(nx, 1);
-	arma::mat sumH = zeros<mat>(nx, nx);
-	arma::mat sumM = zeros<mat>(nx, nx);
-	for (int i = 0; i < N; i++) {
-		arma::vec ym = zeros<vec>(nt(i));
-		arma::mat bigD = zeros<mat>(nt(i), nx);
-		for (int j = 0; j < nt(i); j++) {
-			ym(j) = y(j + index(i)) -mu(j + index(i));
-			for (int k = 0; k < nx; k++) {
-	      bigD(j, k) = muEta(j + index(i)) * X(j + index(i), k);
-			}
-		}
-		arma::mat RhatSlice = Rhat.slice(i);
-		arma::mat bigV = RhatSlice(span(0, nt(i)-1), span(0, nt(i)-1));
-		arma::mat tempS = bigD.t() * pinv(bigV) * ym;
-		sumS = sumS + tempS;
-		arma::mat SRhat = pinv(bigV);
-		arma::mat tempH = bigD.t() * SRhat * bigD;
-		sumH = sumH + tempH;
-		arma::mat tempM = bigD.t() * SRhat * ym * ym.t() * SRhat * bigD;
-		sumM = sumM +tempM;
-	}
-	out(0) = sumS;
-	out(1) = sumH;
-	out(2) = sumM;
-	out.names() = Rcpp::CharacterVector::create("sumS", "sumH", "sumM");    
-	return out;
+Rcpp::List SHM(arma::vec y,
+	       arma::vec mu,
+	       arma::mat X,
+	       arma::cube Rhat, 
+	       arma::vec nt,
+	       arma::vec index){
+  int N = nt.n_elem;
+  int nx = X.n_cols;
+  Rcpp::List out(3);
+  arma::mat sumS(nx, 1, arma::fill::zeros);
+  arma::mat sumH(nx, nx, arma::fill::zeros);
+  arma::mat sumM(nx, nx, arma::fill::zeros);
+  arma::vec ym = y - mu;
+  // arma::mat bigD = matvec(X, muEta);
+  for (int i = 0; i < N; i++) {
+    arma::vec ym2 = ym(span(index(i), index(i) + nt(i) - 1));
+    arma::mat bigD = X.rows(span(index(i), index(i) + nt(i) - 1));
+    arma::mat RhatSlice = Rhat.slice(i);
+    arma::mat bigV = RhatSlice(span(0, nt(i) - 1), span(0, nt(i) - 1));
+    arma::mat tmp = bigD.t() * pinv(bigV);
+    sumS += tmp * ym2;
+    sumH += tmp * bigD;
+    tmp *= ym2;
+    sumM += tmp * tmp.t();
+  }
+  out(0) = sumS;
+  out(1) = sumH;
+  out(2) = sumM;
+  out.names() = Rcpp::CharacterVector::create("sumS", "sumH", "sumM");    
+  return out;
 }
 
 // [[Rcpp::export(rng = false)]]
-Rcpp::List SHM2(arma::vec y,
-								arma::vec mu, arma::mat X,
-								arma::cube Rhat, int N, int nx, arma::vec nt,
-								arma::vec index, arma::vec muEta){
-	Rcpp::List out(3);      
-	arma::mat sumS(nx, 1, arma::fill::zeros);
-	arma::mat sumH(nx, nx, arma::fill::zeros);
-	arma::mat sumM(nx, nx, arma::fill::zeros);
-	arma::vec ym = y - mu;
-	arma::mat bigD = matvec(X, muEta);
-	for (int i = 0; i < N; i++) {
-		arma::vec ym2 = ym(span(index(i), index(i) + nt(i) - 1));
-		arma::mat bigD2 = bigD.rows(span(index(i), index(i) + nt(i) - 1));
-		arma::mat RhatSlice = Rhat.slice(i);
-		arma::mat bigV = RhatSlice(span(0, nt(i) - 1), span(0, nt(i) - 1));
-		sumS += bigD2.t() * pinv(bigV) * ym2;
-		arma::mat SRhat = pinv(bigV);
-		sumH += bigD2.t() * SRhat * bigD2;
-		sumM += bigD2.t() * SRhat * ym2 * ym2.t() * SRhat * bigD2;
-	}
-	out(0) = sumS;
-	out(1) = sumH;
-	out(2) = sumM;
-	out.names() = Rcpp::CharacterVector::create("sumS", "sumH", "sumM");    
-	return out;
+arma::vec qscad(arma::vec b, double lambda) {
+  double a = 3.7;
+  arma::vec dif = a * lambda - b;
+  dif.elem(find(dif < 0)).zeros();
+  arma::vec out(b.n_elem);
+  out.fill(lambda);
+  arma::uvec ind = find(b > lambda);
+  out(ind) = dif(ind) / (a - 1);
+  return(out);
 }
+
+// [[Rcpp::export(rng = false)]]
+Rcpp::List SHEM(arma::vec y,
+		arma::mat X,
+		arma::vec b0,
+		arma::cube Rhat,
+		arma::vec nt,
+		arma::vec pindex, // 0 means to penalize
+		double lambda,
+		double eps){
+  Rcpp::List out(4);
+  int N = nt.n_elem;
+  int nx = X.n_cols;
+  arma::vec index(N, arma::fill::zeros);
+  index(span(1, N - 1)) = cumsum(nt(span(0, N - 2)));
+  arma::vec eta = X * b0;
+  arma::vec E1 = qscad(abs(b0), lambda) / (abs(b0) + eps);
+  E1.elem(find(pindex > 0)).zeros();
+  arma::mat E = diagmat(E1);
+  arma::mat S(nx, 1, arma::fill::zeros);
+  arma::mat H(nx, nx, arma::fill::zeros);
+  arma::mat M(nx, nx, arma::fill::zeros);
+  arma::vec ym = y - eta;  
+  for (int i = 0; i < N; i++) {
+    arma::vec ym2 = ym(span(index(i), index(i) + nt(i) - 1));
+    arma::mat bigD = X.rows(span(index(i), index(i) + nt(i) - 1));
+    arma::mat RhatSlice = Rhat.slice(i);
+    arma::mat bigV = RhatSlice(span(0, nt(i) - 1), span(0, nt(i) - 1));
+    arma::mat tmp = bigD.t() * pinv(bigV);
+    S += tmp * ym2;
+    H += tmp * bigD;
+    tmp *= ym2;
+    M += tmp * tmp.t();
+  } 
+  out(0) = S;
+  out(1) = H;
+  out(2) = E;
+  out(3) = M;
+  out.names() = Rcpp::CharacterVector::create("S", "H", "E", "M");    
+  return out;
+}
+
+// [[Rcpp::export(rng = false)]]
+Rcpp::List gee(arma::vec y,
+	       arma::mat X,
+	       arma::vec b0,
+	       arma::cube Rhat,
+	       arma::vec nt,
+	       arma::vec pindex, // 0 means to penalize
+	       double lambda, double eps,
+	       double tol, int maxit){
+  Rcpp::List out(6);
+  int N = nt.n_elem;
+  int nx = X.n_cols;
+  arma::vec index(N, arma::fill::zeros);
+  index(span(1, N - 1)) = cumsum(nt(span(0, N - 2)));
+  arma::vec b1 = b0;
+  for (int k = 0; k < maxit; k++) {
+    arma::vec eta = X * b0;
+    arma::vec E1 = qscad(abs(b0), lambda) / (abs(b0) + eps);
+    E1.elem(find(pindex > 0)).zeros();
+    arma::mat E = diagmat(E1);
+    arma::mat S(nx, 1, arma::fill::zeros);
+    arma::mat H(nx, nx, arma::fill::zeros);
+    arma::mat M(nx, nx, arma::fill::zeros);
+    arma::vec ym = y - eta;  
+    for (int i = 0; i < N; i++) {
+      arma::vec ym2 = ym(span(index(i), index(i) + nt(i) - 1));
+      arma::mat bigD = X.rows(span(index(i), index(i) + nt(i) - 1));
+      arma::mat RhatSlice = Rhat.slice(i);
+      arma::mat bigV = RhatSlice(span(0, nt(i) - 1), span(0, nt(i) - 1));
+      arma::mat tmp = bigD.t() * pinv(bigV);
+      S += tmp * ym2;
+      H += tmp * bigD;
+      tmp *= ym2;
+      M += tmp * tmp.t();
+    }
+    b1 = b0 + pinv(H + N * E) * (S - N * E * b0);
+    out(0) = b1;
+    out(1) = S;
+    out(2) = H;
+    out(3) = E;
+    out(4) = M;
+    out(5) = k;
+    if(min(abs(b1 - b0)) < tol) break;
+    b0 = b0;
+  }
+  out.names() = Rcpp::CharacterVector::create("b", "S", "H", "E", "M", "iter");
+  return out;
+}
+
+//  
